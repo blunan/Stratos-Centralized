6dfbb4f382757b56734093b6b1dc0788ff168f97
Brayan Luna <blun4n@gmail.com>
2016-05-13 13:52:05 -0500

    Search Schedule Header
    Special header for schedule responses

---
+++ code/search-schedule-header.h
@@ -0,0 +1,41 @@
+#ifndef SEARCH_SCHEDULE_HEADER_H
+#define SEARCH_SCHEDULE_HEADER_H
+
+#include "ns3/header.h"
+#include "ns3/internet-module.h"
+
+#include "definitions.h"
+
+using namespace ns3;
+
+class SearchScheduleHeader : public Header {
+
+	public:
+		static TypeId GetTypeId();
+		virtual TypeId GetInstanceTypeId() const;
+		virtual uint32_t GetSerializedSize() const;
+		virtual void Print(std::ostream &stream) const;
+		virtual uint32_t Deserialize(Buffer::Iterator start);
+		virtual void Serialize(Buffer::Iterator serializer) const;
+
+	private:
+		int serializedScheduleSize;
+
+		double requestTimestamp;
+		Ipv4Address requestAddress;
+		std::list<SearchResponseHeader> schedule;
+
+	public:
+		SearchScheduleHeader();
+
+		double GetRequestTimestamp();
+		Ipv4Address GetRequestAddress();
+		std::list<SearchResponseHeader> GetSchedule();
+
+		void SetRequestTimestamp(double requestTimestamp);
+		void SetRequestAddress(Ipv4Address requestAddress);
+		void SetSchedule(std::list<SearchResponseHeader> schedule);
+};
+std::ostream & operator<< (std::ostream & stream, SearchScheduleHeader const & scheduleHeader);
+
+#endif
--- code/type-header.cc
+++ code/type-header.cc
@@ -41,6 +41,9 @@ void TypeHeader::Print(std::ostream &stream) const {
 		case STRATOS_SEARCH_NOTIFICATION:
 			stream << "Search Notification Message";
 			break;
+		case STRATOS_SEARCH_SCHEDULE:
+			stream << "Search Schedule Message";
+			break
 		default:
 			stream << "Unknown Message";
 	}
